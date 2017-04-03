7177dc3efb0b1925e4231962f7e728063e3aabb3
Melvin Gruschow and Minh Phuong <melvin+minh@>
2017-04-03 10:24:44 -0700

    create injectible service to pass albums around dynamic pages

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
-import { ALBUMS } from './mock-albums';
+import { ALBUMS } from './mock-album';
 
 
 @Injectable()
@@ -10,6 +10,14 @@ export class AlbumService {
 
   getAlbums() {
     return ALBUMS;
+  }
+
+  getAlbumById(albumId: number){
+    for (var i = 0; i <= ALBUMS.length - 1; i++) {
+      if (ALBUMS[i].id === albumId) {
+        return ALBUMS[i];
+      }
+    }
   }
 
 }
